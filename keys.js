f468707aa9f4e7f5f778e11aae867ba658e7589b
G I <tronnie@Gs-MacBook-Pro.local>
2017-09-26 10:23:05 -0700

    twitter keys and files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '	dCh5XIfQFrXlg9ZNLmiCqUSka',
+  consumer_secret: 'Ka1kBSWmmrRDegrtfsWU4RED9fv1HCx76pPXCkmytwdJ3qeVbE',
+  access_token_key: '	911681301685575680-DZKgYQgK9nkK30RMo4f2gLYL7n2lVQJ',
+  access_token_secret: 'UQXH6CX2E28fKyXf38ImvD69M7pL0f3FtFTJmruAnnuu1',
+}
+
+module.exports = twitterKeys;
